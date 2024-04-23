4b3ce794003c6837204d99a6ee82a4bd9127b19e
Poom <papangkorn.poomm@gmail.com>
2024-04-23 17:35:15 +0700

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
-import {createRouter, createWebHistory} from "vue-router"
+import {createRouter, createMemoryHistory} from "vue-router"
+import Table from "@/components/Table.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{}],
+  history: createMemoryHistory(),
+  routes,
+})
+
+const routes = createRouter({
+  routes: [{path: "/table", component: Table, name: "table"}],
 })
 
 export default router
