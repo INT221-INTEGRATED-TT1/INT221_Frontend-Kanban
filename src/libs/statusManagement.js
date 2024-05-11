a5e5ee156a00637be1befe7c9e741dc49af54139
Poom <papangkorn.poomm@gmail.com>
2024-05-11 21:53:57 +0700

    [Fix] : bug StatusManagement

--- src/libs/statusManagement.js
+++ src/libs/statusManagement.js
@@ -22,7 +22,7 @@ class StatusManagement {
   deleteTransferStatus(oldId, newId) {
     let index = []
 
-    const findId = this.statuses.forEach((status, indexId) => {
+    this.statuses.forEach((status, indexId) => {
       if (status.id === oldId) {
         index.push(indexId)
       }
@@ -32,13 +32,9 @@ class StatusManagement {
       this.statuses[id].id = newId
     })
 
-    // if (findId !== -1) {
-    //   this.statuses.splice(findId, 1)
-    // }
-
     index.reverse().forEach((id) => {
-      this.statuses.splice(id, 1);
-  });
+      this.statuses.splice(id, 1)
+    })
 
     // console.log(this.statuses)
   }
